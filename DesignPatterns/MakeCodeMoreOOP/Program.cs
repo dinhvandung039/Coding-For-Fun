a057e8f8983c4ad122a1707d7eccf26554e95380
dung dinh <dinhvandung039@gmail.com>
2023-04-22 23:00:21 +0700

    restructure folder

---
+++ DesignPatterns/MakeCodeMoreOOP/Program.cs
@@ -0,0 +1,15 @@
+﻿//using MakeCodeMoreOOP.Implementations;
+
+//static void Something(ScheduleBase schedule)
+//{
+//    schedule.IsDue();
+//}
+
+//static void Something2(ScheduleBase schedule)
+//{
+//    schedule.IsDue();
+//}
+
+using MakeCodeMoreOOP.Implementations;
+
+ScheduleBase schedule = new ScheduleBase(new DialySchedule());
