5a8e37f602798f321f330fecafe2553e1aab5601
dung dinh <dinhvandung039@gmail.com>
2023-04-25 11:24:41 +0700

    singleton pattern

---
+++ DesignPatterns/SingletonPattern/Interrfaces/IngredientsDbConnectionPool.cs
@@ -0,0 +1,54 @@
+﻿using RealisticDependencies.Implementations;
+using RealisticDependencies.Interfaces;
+
+namespace SingletonPattern.Interrfaces
+{
+    public class IngredientsDbConnectionPool
+    {
+        private readonly Database _database;
+        private readonly IApplicationLogger _logger;
+        private int _openConnect = 0;
+
+        private IngredientsDbConnectionPool(Database database, IApplicationLogger logger)
+        {
+            _database = database;
+            _logger = logger;
+        }
+
+        private static readonly Lazy<IngredientsDbConnectionPool> _instance = new(() =>
+        {
+            var logger = new ConsoleLogger();
+            var database = new Database("connectionstring", logger);
+
+            return new IngredientsDbConnectionPool(database, logger);
+        });
+
+        public static IngredientsDbConnectionPool Instance = _instance.Value;
+
+        public async Task Connect(string client)
+        {
+            if (_openConnect >= 5)
+            {
+                _logger.LogError($"Error - Cannot not acquire new connection. Max connections of {_openConnect}");
+                return;
+            }
+
+            _openConnect++;
+            _logger.LogInfo($"Added connection pool from: {client}", ConsoleColor.Blue);
+            await _database.Connect(client);
+        }
+
+        public async Task Disconnect()
+        {
+            if (_openConnect <= 0)
+            {
+                _logger.LogError($"There are no connections");
+                return;
+            }
+
+            _openConnect--;
+            _logger.LogInfo($"Release connection. Now managing ({_openConnect})", ConsoleColor.Blue);
+            await _database.Disconnect();
+        }
+    }
+}
---
+++ DesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using SingletonPattern.Interrfaces;
+
+const string EastClientId = "US-East";
+const string WestClientId = "US-West";
+const string NorthClientId = "US-North";
+const string SouthClientId = "US-South";
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("🥦  Connecting to ingredients database...");
+var eastClient = IngredientsDbConnectionPool.Instance;
+var westClient = IngredientsDbConnectionPool.Instance;
+var northClient = IngredientsDbConnectionPool.Instance;
+var southClient = IngredientsDbConnectionPool.Instance;
+
+// Open and close connections against the ConnectionPool
+await eastClient.Connect(EastClientId);
+await westClient.Connect(WestClientId);
+await northClient.Connect(NorthClientId);
+await southClient.Connect(SouthClientId);
+
+await northClient.Disconnect();
+await southClient.Connect(SouthClientId);
+
+await northClient.Disconnect();
+await northClient.Disconnect();
+await northClient.Disconnect();
+await northClient.Disconnect();
+
+Console.WriteLine("🥦  Session complete!");
