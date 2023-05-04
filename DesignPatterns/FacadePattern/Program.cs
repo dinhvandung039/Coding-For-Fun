533e9ed27bf440d4c251a344aad1e406e4221078
dung dinh <dinhvandung039@gmail.com>
2023-05-05 00:15:32 +0700

    facade pattern

---
+++ DesignPatterns/FacadePattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using FacadePattern;
+
+
+/// Here we provide a simple Facade to work with more complex underlying logic provided by
+/// a complex system involving much more business logic.  The facade provides a simple interface
+/// to a complex subsystem. 
+/// 
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("🛍 Welcome to the Grocery Reporting System");
+
+var reporter = new DailyReporter();
+
+reporter.KickOffProduceReport();
---
+++ DesignPatterns/FacadePattern/VendorNotifier.cs
@@ -0,0 +1,49 @@
+﻿using AbstractFactory.Interfaces;
+using RealisticDependencies.Interfaces;
+
+namespace FacadePattern
+{
+    public interface IVendorNotifier
+    {
+        Task NotifyVendorOfCurrentStock(string vendor);
+        List<string> GetVendorsForDepartment(string dept);
+    }
+
+    public class VendorNotifier : IVendorNotifier
+    {
+        private readonly IDatabase _database;
+        private readonly ISendEmail _emailer;
+
+        public VendorNotifier(
+            IDatabase database,
+            ISendEmail emailer)
+        {
+            _database = database;
+            _emailer = emailer;
+        }
+
+        public Task NotifyVendorOfCurrentStock(string vendor)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Notifying vendor: {vendor}");
+            Console.ResetColor();
+            return Task.CompletedTask;
+        }
+
+        public List<string> GetVendorsForDepartment(string department)
+        {
+            // Could use its own dependencies, like the database
+
+            if (department == "produce")
+            {
+                return new List<string> {
+                    "Organic Orchards",
+                    "McKinnon Farm",
+                    "Pleasant Valley Farms"
+                };
+            }
+
+            return new List<string>();
+        }
+    }
+}
