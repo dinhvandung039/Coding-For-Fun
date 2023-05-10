06aad28bde24bb99dd4539c5379bbc60adc231b2
dung dinh <dinhvandung039@gmail.com>
2023-05-11 00:12:38 +0700

    iterator pattern

---
+++ DesignPatterns/Iterator/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿namespace Iterator;
+
+public class LinkedListIterator<T>
+{
+    public Node _noded { get; set; }
+    public class Node
+    {
+        public Node Next { get; set; }
+        public T Value { get; set; }
+
+        public void Append(T value)
+        {
+            if (Next == null)
+            {
+                Next = new Node { Value = value };
+            }
+
+            Next.Append(value);
+        }
+
+        public T Get(int i) => i == 0 ? Value : Next.Get(--i);
+    }
+
+    public void Add(T value)
+    {
+        if (_noded == null)
+        {
+            _noded = new Node { Value = value };
+            return;
+        }
+
+        _noded.Append(value);
+    }
+
+    public T Get(int i) => _noded.Get(i);
+}
---
+++ DesignPatterns/Iterator/Program.cs
@@ -0,0 +1,15 @@
+﻿/// Iterator is a behavioral pattern that allows sequential traversal through a complex data structure without its internal details
+/// 
+using Iterator;
+
+var list = new LinkedListIterator<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+list.Add(6);
+list.Add(7);
+Console.WriteLine(list);
+
+Console.WriteLine(list.Get(3));
